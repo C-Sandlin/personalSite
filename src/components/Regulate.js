dd449fcc689756df4483ec29b27c6424458ff15b
Colin Sandlin <csandlin010@gmail.com>
2019-07-08 14:27:30 -0500

    added icons

--- src/components/Regulate.js
+++ src/components/Regulate.js
@@ -17,6 +17,12 @@ import { DiHtml5, DiCss3, DiReact, DiGithubBadge, DiJsBadge } from "react-icons/
 import ScrollAnimation from 'react-animate-on-scroll';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
+import firebasesvg from '../img/firebase.svg';
+import leafletsvg from '../img/Leaflet.svg';
+import chartjssvg from '../img/chartjs.svg';
+import momentjssvg from '../img/momentjs.svg';
+import reactjssvg from '../img/reactjs_tiny.svg';
+import herejssvg from "../img/herejs.svg";
 
 const images = [
     'https://camo.githubusercontent.com/1b039f92a53a78cae7aabc86cf657efda7a32a8a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f636f6c696e732d63617073746f6e652d313535383536353236323734392e61707073706f742e636f6d2f6f2f53637265656e25323053686f74253230323031392d30372d3031253230617425323031302e30322e3537253230414d2e706e673f616c743d6d6564696126746f6b656e3d61396463636532662d643434332d343535362d623338632d376561656464303835373165',
@@ -48,16 +54,24 @@ export default class Regulate extends Component {
                                 <p className="project-text">I created Regulate to help both clients and therapists as they work together to improve their mental health. My wife is a therapist and together we realized the need for an intuitive, intelligent application to help clients track data, recall coping techniques, and find support. Over time, this data can make a significant impact in how clients progress and manage their unique situations.</p>
 
                                 <IconContext.Provider value={{ size: '1.5rem' }}>
-                                    <a className="readmore" href="http://www.github.com/c-sandlin" target="_blank">Read More on Github <IoIosArrowRoundForward style={{ marginBottom: '-8px' }} /></a>
+                                    <a className="readmore" href="https://github.com/C-Sandlin/capstone-deployed" target="_blank">Read More on Github <IoIosArrowRoundForward style={{ marginBottom: '-8px' }} /></a>
                                 </IconContext.Provider>
 
                                 <div className="techs-container">
                                     <div className="techs-icons">
-                                        <p className="tooltip"><DiHtml5 className="techicon"></DiHtml5><span className="tooltiptext2">HTML5</span></p>
-                                        <p className="tooltip"><DiCss3 className="techicon" ></DiCss3><span className="tooltiptext2">CSS3</span></p>
-                                        <p className="tooltip"><DiJsBadge className="techicon" ></DiJsBadge><span className="tooltiptext2">Javascript</span></p>
-                                        <p className="tooltip"><DiReact className="techicon" ></DiReact><span className="tooltiptext2">ReactJs</span></p>
-                                        <p className="tooltip"><DiGithubBadge className="techicon" ></DiGithubBadge><span className="tooltiptext2">Github</span></p>
+                                        <IconContext.Provider value={{ size: '1.6rem', style: { verticalAlign: 'middle' } }}>
+                                            <p className="tooltip"><DiHtml5 className="techicon" /><span className="tooltiptext2">HTML5</span></p>
+                                            <p className="tooltip"><DiCss3 className="techicon" /><span className="tooltiptext2">CSS3</span></p>
+                                            <p className="tooltip"><DiJsBadge className="techicon" /><span className="tooltiptext2">Javascript</span></p>
+                                            {/* <p className="tooltip"><DiReact id="reactjssvg" className="techicon" /><span className="tooltiptext2">ReactJs</span></p> */}
+                                            <p className="tooltip"><img id="reactjs" className={["techicon", "colin-icon"].join(' ')} src={reactjssvg} alt="firebase logo"></img><span className="tooltiptext2">React Js</span></p>
+                                            <p className="tooltip"><img className={["techicon", "colin-icon"].join(' ')} src={firebasesvg} alt="firebase logo"></img><span className="tooltiptext2">Firebase</span></p>
+                                            <p className="tooltip"><img className={["techicon", "colin-icon"].join(' ')} src={leafletsvg} alt="firebase logo"></img><span className="tooltiptext2">Leaflet.js</span></p>
+                                            <p className="tooltip"><img className={["techicon", "colin-icon"].join(' ')} src={chartjssvg} alt="firebase logo"></img><span className="tooltiptext2">Chart.js</span></p>
+                                            <p className="tooltip"><img id="momentjs" className={["techicon", "colin-icon"].join(' ')} src={momentjssvg} alt="firebase logo"></img><span className="tooltiptext2">Moment.js</span></p>
+                                            <p className="tooltip"><img id="momentjs" className={["techicon", "colin-icon"].join(' ')} src={herejssvg} alt="firebase logo"></img><span className="tooltiptext2">Here.js</span></p>
+                                            <p className="tooltip"><IconContext.Provider value={{ size: '1.8rem', style: { verticalAlign: 'middle' } }}><DiGithubBadge id="github" className="techicon" /></IconContext.Provider><span className="tooltiptext2">Github</span></p>
+                                        </IconContext.Provider>
                                     </div>
                                 </div>
                             </div>
